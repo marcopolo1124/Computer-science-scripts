daad072844e1bb3e453fe341d14c278a038f4f1b
marcopolo1124 <85742840+marcopolo1124@users.noreply.github.com>
2021-08-30 00:14:09 +0100

    added sieve of erastothnes and linked list

---
+++ Linked list.py
@@ -0,0 +1,202 @@
+class Node:
+    """
+    An object for storing a single node of a linked list.
+    Models two attributes - data and the link to the next node in the list
+    """
+
+    data = None
+    next_node = None
+    
+    def __init__(self, data):
+        self.data =data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+class LinkedList:
+    """
+    Singly linked list
+    """
+    def __init__(self):
+        self.head = None
+
+
+    def lst_to_link(self,lst):
+        for i in range(len(lst)):
+            lst[i] = Node(lst[i])
+        
+        for i in range(len(lst)):
+            if i!= len(lst) - 1:
+                lst[i].next_node = lst[i+1]
+        self.head = lst[0]
+
+
+
+    def is_empty(self):
+        """
+        Returns whether the linked list is empty or not
+        """
+        return self.head == None
+    
+    def size(self):
+        """
+        Returns the number of nodes in the list.
+        Take O(n) time
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
+
+    def add(self,data):
+        """
+        Adds new Node containing data at the head of the list
+        Takes constant time
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+    
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+        Return the node or 'None' if not found
+        """
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data,index):
+        """
+        Inserts a new Node containing data at index position
+        Insertion take O(1) time but finding the node at the
+        insertion point takes O(n) time.
+
+        Takes overall O(n) time
+        """
+        if index == 0:
+            self.add(data)
+
+        if index > 0:
+            new = Node(data)
+            position = index
+            current = self.head
+            while position > 1:
+                current = current.next_node
+                position -= 1
+            
+            prev_node = current
+            next_node = current.next_node 
+
+            prev_node.next_node = new
+            new.next_node = next_node
+
+    def append(self, data):
+        size= self.size()
+        if size > 0:
+            self.insert(data, size)
+        if size == 0:
+            self.head = data
+
+    def delete(self, index):
+        """
+        Removes the node at the index position
+        """
+        if index == 0:
+            self.head = self.head.next_node
+        else:
+            current = self.head
+            position = index
+            while position > 1:
+                current = current.next_node
+                position -= 1
+
+            prev_node = current
+            next_node = current.next_node.next_node
+
+            prev_node.next_node = next_node
+
+    def delete_by_key(self, key):
+        """
+        Removes node containing data that matches the key
+        Takes O(n) time
+        """
+
+        current = self.head
+        previous = None
+        found = False
+        
+        while current and not found:
+            if current.data == key and current == self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+        return current
+    
+    def __iter__(self):
+        self.current_node = self.head
+        return self
+
+    def __next__(self):
+        if self.current_node is not None:
+            return_val = self.current_node
+            self.current_node = self.current_node.next_node
+            return return_val
+        else:
+            raise StopIteration
+
+    def __getitem__(self, index):
+        current = self.head
+        if index == 0:
+            return self.head
+        if index > 0:
+            for i in range(index):
+                if current is not None:
+                    current = current.next_node
+                else:
+                    raise IndexError('Index out of range')
+
+        if current is None:
+            raise IndexError('Index out of range')
+        else:
+            return current
+
+    def __repr__(self):
+        nodes = []
+        current = self.head
+        while current:
+            if current is self.head:
+                nodes.append("[Head:%s]"% current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail:%s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+
+            current = current.next_node
+        return '->'.join(nodes)
+
+
+
+
+
+ll1 = LinkedList()
+ll1.lst_to_link([1,2,3])
+print(ll1)
+
+
+ll1.append('I am a cat')
+ll1.delete_by_key(1)
+print(ll1)
